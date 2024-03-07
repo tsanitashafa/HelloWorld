e333f7f0e435e609bcb0fd7ff6e82345a5876dd8
tsanitashafa <tsanitashafa@gmail.com>
2024-03-07 17:34:23 +0700

    Halo ini commit pertama saya

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main (String[] args) {
+     System.out.println("     /\\_______/\\    ");
+     System.out.println("    /   0   0   \\    ");
+     System.out.println("   (  ==  ^  ==  )   ");
+     System.out.println("    )           (    ");
+     System.out.println("   (             )   ");
+     System.out.println("  (  (  )   (  )  )  ");
+     System.out.println(" (___(__)___(__)___) ");
+
+
+    }
+}
